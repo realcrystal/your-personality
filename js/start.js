6e9cda9c71e92e17f18c20c409d9e098841bf834
realcrystal <realcrystal98@gmail.com>
2021-06-24 22:08:04 +0900

    Add qna backend functions

--- js/start.js
+++ js/start.js
@@ -2,6 +2,30 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 
+function addAnswer(answerText, qIdx){
+  var a = document.querySelector(".aBox");
+  var answer = document.createElement("button");
+  answer.classList.add("answerList");
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll(".answerList");
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.display = "none";
+    }
+    if(qIdx < 11) goNext(++qIdx);
+  }, false);
+}
+
+function goNext(qIdx){
+  var q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
 function begin(){
   main.style.WebkitAnimation = "fadeOut 0.5s";
   main.style.animation = "fadeOut 0.5s";
@@ -13,4 +37,6 @@ function begin(){
       qna.style.display = "block";
     }, 200);
   }, 200);
+  let qIdx = 0;
+  goNext(qIdx);
 }
